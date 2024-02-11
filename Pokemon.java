40fcd31ab055a306eedb4235fb919faf9c5df760
OeksiiYavorskyi <158568548+OeksiiYavorskyi@users.noreply.github.com>
2024-02-11 19:20:15 +0200

    Update Pokemon.java
    Signed-off-by: OeksiiYavorskyi <158568548+OeksiiYavorskyi@users.noreply.github.com>

--- Pokemon.java
+++ Pokemon.java
@@ -1,34 +1,75 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Pikachu pikachu = new Pikachu("Пикачу");
-        Charmander charmander = new Charmander("Чармандер");
+        ArrayList<Pokemon> team1 = new ArrayList<>();
+        ArrayList<Pokemon> team2 = new ArrayList<>();
 
-        pikachu.fight(charmander);
-        charmander.fight(pikachu);
-        pikachu.sleep();
-        charmander.sleep();
+        // Создаем покемонов для первой команды
+        team1.add(new Pikachu("Пикачу"));
+        team1.add(new Charmander("Чармандер"));
+        team1.add(new Squirtle("Сквиртл"));
+
+        // Создаем покемонов для второй команды
+        team2.add(new Bulbasaur("Бульбазавр"));
+        team2.add(new Jigglypuff("Джигглипафф"));
+        team2.add(new Meowth("Мяут"));
+
+        battleTeams(team1, team2);
+    }
+
+    public static void battleTeams(ArrayList<Pokemon> team1, ArrayList<Pokemon> team2) {
+        for (int i = 0; i < team1.size(); i++) {
+            Pokemon pokemon1 = team1.get(i);
+            Pokemon pokemon2 = team2.get(i);
+
+            System.out.println("---- Начинает бой " + pokemon1.getName() + " против " + pokemon2.getName() + " ----");
+            pokemon1.fight(pokemon2);
+            pokemon2.fight(pokemon1);
+            pokemon1.sleep();
+            pokemon2.sleep();
+        }
     }
 }
 
 class Pikachu extends Pokemon {
-    // Конструктор класса Pikachu
     public Pikachu(String name) {
         super(name, 100, 55, 40, 50, 50, 90);
     }
 }
 
-// Определение класса Charmander
 class Charmander extends Pokemon {
-    // Конструктор класса Charmander
     public Charmander(String name) {
         super(name, 90, 52, 43, 60, 50, 65);
     }
 }
 
+class Squirtle extends Pokemon {
+    public Squirtle(String name) {
+        super(name, 80, 48, 65, 50, 64, 43);
+    }
+}
+
+class Bulbasaur extends Pokemon {
+    public Bulbasaur(String name) {
+        super(name, 85, 49, 49, 65, 65, 45);
+    }
+}
+
+class Jigglypuff extends Pokemon {
+    public Jigglypuff(String name) {
+        super(name, 115, 45, 20, 45, 25, 20);
+    }
+}
+
+class Meowth extends Pokemon {
+    public Meowth(String name) {
+        super(name, 40, 45, 35, 40, 40, 90);
+    }
+}
+
 class Pokemon {
-    // Переменные экземпляра
     String name;
     int healthPoints;
     int attack;
@@ -37,17 +78,14 @@ class Pokemon {
     int specialDefense;
     int speed;
 
-    // Статическое поле для хранения дополнительного объема HP
-    public final static double additionalHP;
+    private final static double additionalHP;
 
-    // Статический инициализатор для установки значения additionalHP из пользовательского ввода
     static {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите дополнительное количество HP для всех покемонов:");
         additionalHP = scanner.nextDouble();
     }
 
-    // Конструктор класса Pokemon
     public Pokemon(String name, int healthPoints, int attack, int defense, int specialAttack, int specialDefense, int speed) {
         this.name = name;
         this.healthPoints = healthPoints;
@@ -58,66 +96,48 @@ class Pokemon {
         this.speed = speed;
     }
 
-    // Метод для боя
-    public void fight(Pokemon opponent) {
-        System.out.println(name + " начинает бой против " + opponent.getName() + "!");
-
-        // Определение урона, который нанесет текущий покемон
-        int damage = calculateDamage(opponent);
-
-        // Уменьшение здоровья оппонента на величину урона
-        opponent.reduceHealthPoints(damage);
-
-        System.out.println(name + " нанес " + damage + " урона " + opponent.getName() + ".");
-        System.out.println(opponent.getName() + " осталось здоровья: " + opponent.getHealthPoints());
-
-        // Проверяем здоровье оппонента после удара
-        if (opponent.getHealthPoints() <= 0) {
-            System.out.println(opponent.getName() + " был побежден!");
-            return;
-        }
-
-        // Оппонент атакует в ответ
-        damage = opponent.calculateDamage(this);
-        this.reduceHealthPoints(damage);
-
-        System.out.println(opponent.getName() + " нанес " + damage + " урона " + name + ".");
-        System.out.println(name + " осталось здоровья: " + getHealthPoints());
-
-        // Проверяем здоровье текущего покемона после удара оппонента
-        if (getHealthPoints() <= 0) {
-            System.out.println(name + " был побежден!");
-        }
-    }
-
-    // Метод для сна
-    public void sleep() {
-        System.out.println(name + " засыпает и восстанавливает здоровье.");
-        // Восстанавливаем здоровье на 20% от максимального значения
-        int healAmount = (int) (healthPoints * 0.2);
-        healthPoints += healAmount;
-        System.out.println("Здоровье " + name + " восстановлено на " + healAmount + " единиц.");
-    }
-
-    // Метод для расчета урона, наносимого текущим покемоном
-    private int calculateDamage(Pokemon opponent) {
-        //возвращаем значение атаки текущего покемона
-        return attack;
-    }
-
-    // Метод для уменьшения здоровья покемона на определенную величину
-    public void reduceHealthPoints(int amount) {
-        healthPoints -= amount;
-        if (healthPoints < 0) {
-            healthPoints = 0;
-        }
-    }
     public String getName() {
         return name;
     }
 
     public int getHealthPoints() {
         return healthPoints;
+    }
 
+    public void fight(Pokemon opponent) {
+        System.out.println(name + " начинает бой против " + opponent.getName() + "!");
+        int damage = calculateDamage(opponent);
+        opponent.reduceHealthPoints(damage);
+        System.out.println(name + " нанес " + damage + " урона " + opponent.getName() + ".");
+        System.out.println(opponent.getName() + " осталось здоровья: " + opponent.getHealthPoints());
+        if (opponent.getHealthPoints() <= 0) {
+            System.out.println(opponent.getName() + " был побежден!");
+            return;
+        }
+        damage = opponent.calculateDamage(this);
+        this.reduceHealthPoints(damage);
+        System.out.println(opponent.getName() + " нанес " + damage + " урона " + name + ".");
+        System.out.println(name + " осталось здоровья: " + getHealthPoints());
+        if (getHealthPoints() <= 0) {
+            System.out.println(name + " был побежден!");
+        }
+    }
+
+    public void sleep() {
+        System.out.println(name + " засыпает и восстанавливает здоровье.");
+        int healAmount = (int) (healthPoints * 0.2);
+        healthPoints += healAmount;
+        System.out.println("Здоровье " + name + " восстановлено на " + healAmount + " единиц.");
+    }
+
+    private int calculateDamage(Pokemon opponent) {
+        return attack;
+    }
+
+    public void reduceHealthPoints(int amount) {
+        healthPoints -= amount;
+        if (healthPoints < 0) {
+            healthPoints = 0;
+        }
     }
 }
