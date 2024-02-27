105e865b9088545bf392bb5d57b88b36d2377922
OeksiiYavorskyi <158568548+OeksiiYavorskyi@users.noreply.github.com>
2024-02-27 10:33:11 +0200

    Update BookShelf.java
    Signed-off-by: OeksiiYavorskyi <158568548+OeksiiYavorskyi@users.noreply.github.com>

--- BookShelf.java
+++ BookShelf.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Scanner;
 
 public class BookShelf {
-    private List<Book> books = new ArrayList<>();
+    private BookLinkedList books = new BookLinkedList();
+
     static class Book {
         String title;
         String author;
         int year;
+
         Book(String title, String author, int year) {
             this.title = title;
             this.author = author;
@@ -21,27 +23,85 @@ public class BookShelf {
         }
     }
 
-    void addBook(String title, String author, int year) {
-        books.add(new Book(title, author, year));
+    static class BookNode {
+        Book book;
+        BookNode prev;
+        BookNode next;
+
+        public BookNode(Book book) {
+            this.book = book;
+            this.prev = null;
+            this.next = null;
+        }
     }
 
-    void removeBook(String title) {
-        Iterator<Book> iterator = books.iterator();
-        while (iterator.hasNext()) {
-            Book book = iterator.next();
-            if (book.title.equalsIgnoreCase(title)) {
-                iterator.remove();
-                System.out.println("Книга \"" + title + "\" удалена.");
-                return;
+    static class BookLinkedList implements Iterable<Book> {
+        private BookNode head;
+        private BookNode tail;
+
+        public BookLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void addBook(Book book) {
+            BookNode newNode = new BookNode(book);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                newNode.prev = tail;
+                tail = newNode;
             }
         }
-        System.out.println("Книга \"" + title + "\" не найдена.");
-    }
 
-    void displayBooks() {
-        System.out.println("Список книг на полке:");
-        for (Book book : books) {
-            System.out.println(book);
+        public void removeBook(String title) {
+            BookNode current = head;
+            while (current != null) {
+                if (current.book.title.equalsIgnoreCase(title)) {
+                    if (current.prev != null) {
+                        current.prev.next = current.next;
+                    } else {
+                        head = current.next;
+                    }
+                    if (current.next != null) {
+                        current.next.prev = current.prev;
+                    } else {
+                        tail = current.prev;
+                    }
+                    System.out.println("Книга \"" + title + "\" удалена.");
+                    return;
+                }
+                current = current.next;
+            }
+            System.out.println("Книга \"" + title + "\" не найдена.");
+        }
+
+        public void displayBooks() {
+            System.out.println("Список книг на полке:");
+            for (Book book : this) {
+                System.out.println(book);
+            }
+        }
+
+        @Override
+        public Iterator<Book> iterator() {
+            return new Iterator<Book>() {
+                private BookNode current = head;
+
+                @Override
+                public boolean hasNext() {
+                    return current != null;
+                }
+
+                @Override
+                public Book next() {
+                    Book book = current.book;
+                    current = current.next;
+                    return book;
+                }
+            };
         }
     }
 
@@ -50,16 +110,16 @@ public class BookShelf {
         Scanner scanner = new Scanner(System.in);
 
         // Добавляем книги для 5 разных авторов
-        shelf.addBook("Война и мир", "Лев Толстой", 1869);
-        shelf.addBook("Анна Каренина", "Лев Толстой", 1877);
-        shelf.addBook("Преступление и наказание", "Федор Достоевский", 1866);
-        shelf.addBook("Братья Карамазовы", "Федор Достоевский", 1880);
-        shelf.addBook("1984", "Джордж Оруэлл", 1949);
-        shelf.addBook("Ферма животных", "Джордж Оруэлл", 1945);
-        shelf.addBook("Мастер и Маргарита", "Михаил Булгаков", 1967);
-        shelf.addBook("Собачье сердце", "Михаил Булгаков", 1925);
-        shelf.addBook("Унесенные ветром", "Маргарет Митчелл", 1936);
-        shelf.addBook("Бегущий по лезвию", "Филип К. Дик", 1968);
+        shelf.books.addBook(new Book("Война и мир", "Лев Толстой", 1869));
+        shelf.books.addBook(new Book("Анна Каренина", "Лев Толстой", 1877));
+        shelf.books.addBook(new Book("Преступление и наказание", "Федор Достоевский", 1866));
+        shelf.books.addBook(new Book("Братья Карамазовы", "Федор Достоевский", 1880));
+        shelf.books.addBook(new Book("1984", "Джордж Оруэлл", 1949));
+        shelf.books.addBook(new Book("Ферма животных", "Джордж Оруэлл", 1945));
+        shelf.books.addBook(new Book("Мастер и Маргарита", "Михаил Булгаков", 1967));
+        shelf.books.addBook(new Book("Собачье сердце", "Михаил Булгаков", 1925));
+        shelf.books.addBook(new Book("Унесенные ветром", "Маргарет Митчелл", 1936));
+        shelf.books.addBook(new Book("Бегущий по лезвию", "Филип К. Дик", 1968));
 
         System.out.println("Добро пожаловать в виртуальную книжную полку!");
         while (true) {
@@ -81,16 +141,16 @@ public class BookShelf {
                     System.out.println("Введите год издания книги:");
                     int year = scanner.nextInt();
                     scanner.nextLine(); // Чтобы считать символ новой строки после считывания числа
-                    shelf.addBook(title, author, year);
+                    shelf.books.addBook(new Book(title, author, year));
                     System.out.println("Книга успешно добавлена!");
                     break;
                 case 2:
                     System.out.println("Введите название книги, которую хотите удалить:");
                     String titleToRemove = scanner.nextLine();
-                    shelf.removeBook(titleToRemove);
+                    shelf.books.removeBook(titleToRemove);
                     break;
                 case 3:
-                    shelf.displayBooks();
+                    shelf.books.displayBooks();
                     break;
                 case 4:
                     System.out.println("Выход из программы.");
